f7ce7faaeb66c8eeaa2fe824cd84ce44f48e9c16
Ardelean Andrei <45100902+Ardealul@users.noreply.github.com>
2020-11-19 18:48:04 +0200

    .

--- app/src/main/java/com/ardeal/labandroid/auth/data/AuthRepository.kt
+++ app/src/main/java/com/ardeal/labandroid/auth/data/AuthRepository.kt
@@ -27,15 +27,9 @@ object AuthRepository {
     suspend fun login(username: String, password: String): Result<TokenHolder> {
         val user = User(username, password)
         val result = RemoteAuthDataSource.login(user)
-        Log.v(TAG, "Userul este:")
-        Log.v(TAG, user.toString())
         if (result is Result.Success<TokenHolder>) {
-            Log.v(TAG, "Tokenul este:")
-            Log.v(TAG, result.data.token)
             setLoggedInUser(user, result.data)
             Constants.instance()?.storeValueString("token", result.data.token)
-            Log.v(TAG, "l-a salvat oare? tokenul din constants este:") ///////
-            Log.v(TAG, Constants.instance()?.fetchValueString("token").toString())
         }
         return result
     }
